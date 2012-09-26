c0b2576dcdfddfdf468ea8a5bd199c680fbfd9c3
Kris Bulman <kris@bulman.ca>
2012-09-26 13:09:02 -0300

    removing contents from master branch

--- upei-footer-footer.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="upei-footer">
-  <?php
-    $request = drupal_http_request('http://students.upei.ca/files/misc/footer.php');
-    $data = $request->data;
-    if (isset($data)) {
-      print $data;
-    }
-  ?>
-</div>
